ab7f40fd96c5e23492d5bfcf4c98361358b33133
unknown <dysontilano@gmail.com>
2024-11-24 23:07:20 -0500

    Documentation updated and source code refactorized

--- backend/restaurant-backend-api-java/src/main/java/com/project/dev/api/service/mapping/IngredientMapping.java
+++ backend/restaurant-backend-api-java/src/main/java/com/project/dev/api/service/mapping/IngredientMapping.java
@@ -29,23 +29,21 @@ import org.mapstruct.Mapping;
 public interface IngredientMapping extends GenericMapping<IngredientDTO, Ingredient> {
 
     /**
-     * Obtiene una entidad en base a su DTO.
+     * Obtiene una entidad en base a su DTO usando el campo clave de la base de datos.
      *
      * @param dto es el DTO a convertir en entidad.
      * @return la entidad equivalente al dto.
      */
-    // Deben ser el campo clave de la base de datos.
     @Mapping(source = "intIngredientId", target = "intIngredientId")
     @Override
     public Ingredient getEntity(IngredientDTO dto);
 
     /**
-     * Obtiene un DTO en base a su entidad.
+     * Obtiene un DTO en base a su entidad usando el campo clave de la base de datos.
      *
      * @param entity es la entidad a convertir en DTO.
      * @return el dto equivalente a la entidad.
      */
-    // Deben ser el campo clave de la base de datos.
     @Mapping(source = "intIngredientId", target = "intIngredientId")
     @Override
     public IngredientDTO getDto(Ingredient entity);
